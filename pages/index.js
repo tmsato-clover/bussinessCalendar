491be1801de2dc47ffe3b3107e7f5e94f97772be
tm.sato <tm.sato@968inc.com>
2021-09-08 14:52:09 +0900

    change import

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react"; // useStateを追加
-import eachDayOfInterval from "./date-fns/eachDayOfInterval";
-import endOfWeek from "./date-fns/endOfWeek";
-import eachWeekOfInterval from "./date-fns/eachWeekOfInterval";
-import startOfMonth from "./date-fns/startOfMonth";
-import endOfMonth from "./date-fns/endOfMonth";
+import * as dateFns from "date-fns";
+// import eachDayOfInterval from 'date-fns/eachDayOfInterval'
+// import endOfWeek from 'date-fns/endOfWeek'
+// import eachWeekOfInterval from 'date-fns/eachWeekOfInterval'
+// import startOfMonth from 'date-fns/startOfMonth'
+// import endOfMonth from 'date-fns/endOfMonth'
 
 const getCalendarArray = (date) => {
-  const sundays = eachWeekOfInterval({
-    start: startOfMonth(date),
-    end: endOfMonth(date),
+  const sundays = dateFns.eachWeekOfInterval({
+    start: dateFns.startOfMonth(date),
+    end: dateFns.endOfMonth(date),
   });
   return sundays.map((sunday) =>
-    eachDayOfInterval({ start: sunday, end: endOfWeek(sunday) })
+    dateFns.eachDayOfInterval({ start: sunday, end: dateFns.endOfWeek(sunday) })
   );
 };
 
