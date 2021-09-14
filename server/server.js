c62f23a22ceadd23fb0f18a3ce51ccaa2e629020
tm.sato <tm.sato@968inc.com>
2021-09-14 18:38:30 +0900

    get shopi info from env

--- server/server.js
+++ server/server.js
@@ -115,8 +115,7 @@ app.prepare().then(async () => {
   router.get("(/_next/static/.*)", handleRequest); // Static content is clear
   router.get("/_next/webpack-hmr", handleRequest); // Webpack content is clear
   router.get("(.*)", async (ctx) => {
-    //    const shop = ctx.query.shop;
-    const shop = process.env.shop;
+    const shop = ctx.query.shop;
 
     // This shop hasn't been seen yet, go through OAuth to create a session
     if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
